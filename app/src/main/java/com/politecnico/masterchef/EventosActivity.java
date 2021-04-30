5898e71a2b6f6a5668e38fa53c1d51c06b9e5574
txinpower <51758559+txinpower@users.noreply.github.com>
2021-04-30 12:13:39 +0200

    ejemplo de textfield materialdesign, activity eventos con cardview basico

---
+++ app/src/main/java/com/politecnico/masterchef/EventosActivity.java
@@ -0,0 +1,13 @@
+package com.politecnico.masterchef;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eventos);
+    }
+}
