93b64bfcc714188308e0b85ee9821b9c5b07262d
Jorge <jgselectronico@gmail.com>
2021-05-12 11:47:12 +0200

    validar juez al entrar al evento y su php

---
+++ Conexiones PHP/validarJuezEvento.php
@@ -0,0 +1,15 @@
+<?php
+include 'conexion.php';
+$idjuez=$_POST['idjuez'];
+$idevento=$_POST['idevento'];
+
+$sentencia=$conexion->prepare("SELECT * FROM Solicitudes WHERE ID_juez=$idjuez AND ID_evento=$idevento AND Solicitud='Admitido'");
+$sentencia->execute();
+
+$resultado = $sentencia->get_result();
+if ($fila = $resultado->fetch_assoc()) {
+         echo json_encode($fila,JSON_UNESCAPED_UNICODE);     
+}
+$sentencia->close();
+$conexion->close();
+?>
--- app/src/main/java/com/politecnico/masterchef/DetallesEventoActivity.java
+++ app/src/main/java/com/politecnico/masterchef/DetallesEventoActivity.java
@@ -2,7 +2,9 @@ package com.politecnico.masterchef;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.SpannableString;
@@ -11,8 +13,19 @@ import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DetallesEventoActivity extends BaseAppCompatMenu {
 
@@ -20,12 +33,17 @@ public class DetallesEventoActivity extends BaseAppCompatMenu {
 
     Button btnVolverListado, btnAccederEvento;
 
+    RequestQueue requestQueue;
 
+    String usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalles_evento);
+
+        SharedPreferences preferences = DetallesEventoActivity.this.getSharedPreferences("preferenciasLogin", Context.MODE_PRIVATE);
+        usuario = preferences.getString("id", "");
 
         //obtener el objeto/evento seleccionado
         Evento evento = (Evento) getIntent().getSerializableExtra("evento");
@@ -59,19 +77,49 @@ public class DetallesEventoActivity extends BaseAppCompatMenu {
         btnAccederEvento.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), VotacionActivity.class);
                 //seleccionamos el evento
-                //pasar contenido por intent// clase evento implement serializable para pasar objetos
                 String idevento = evento.getIdEvento()+"";
-                i.putExtra("id_evento", idevento);
-                startActivity(i);
+                validarJuez("http://10.0.2.2/masterchef/validarJuezEvento.php",  usuario, idevento );
+
+
+
             }
         });
 
+    }
 
+    private void validarJuez(String URL, String idjuez, String idevento) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (!response.isEmpty()) {
+                    Toast.makeText(DetallesEventoActivity.this, "Juez validado para el evento", Toast.LENGTH_LONG).show();
+                    //pasar contenido por intent// clase evento implement serializable para pasar objetos
+                    Intent i = new Intent(getApplicationContext(), VotacionActivity.class);
+                    i.putExtra("id_evento", idevento);
+                    startActivity(i);
+                }else {
 
+                    Toast.makeText(DetallesEventoActivity.this, "Juez no autorizado en el evento", Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Toast.makeText(DetallesEventoActivity.this, volleyError.toString(), Toast.LENGTH_LONG).show();
+            }
+        }
 
-
-
+        ) {
+            protected Map<String, String> getParams() throws AuthFailureError {
+                //utilizar objetos en vez de String para tener campos con otro tipo de valores//pendiente
+                Map<String, String> param = new HashMap<String, String>();
+                param.put("idjuez", idjuez);
+                param.put("idevento", idevento);
+                return param;
+            }
+        };
+        requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
